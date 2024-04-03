0627827b4ca2cb176b9f7513a896e50e411be8d1
Krishna vamsi <64204235+krishna6143@users.noreply.github.com>
2024-04-03 12:34:26 +0530

    Add files via upload

---
+++ NumberAnalyzer.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class NumberAnalyzer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        int sum = 0;
+        int count = 0;
+
+        boolean continueInput = true;
+
+        while (continueInput) {
+            System.out.print("Enter a number (enter 0 to stop): ");
+            int number = scanner.nextInt();
+
+            if (number == 0) {
+                continueInput = false;
+            } else {
+                if (number > largest) {
+                    largest = number;
+                }
+                if (number < smallest) {
+                    smallest = number;
+                }
+
+                sum += number;
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("No numbers entered.");
+        } else {
+            double average = (double) sum / count;
+            System.out.println("Largest number entered: " + largest);
+            System.out.println("Smallest number entered: " + smallest);
+            System.out.println("Average of all numbers entered: " + average);
+        }
+
+        scanner.close();
+    }
+}
